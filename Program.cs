b861ccda2ada6c756ecb0ced3cdeb09025b5810e
Matias Fernando Alvarado Pinos <matias2321160@gmail.com>
2026-02-26 02:01:32 -0500

    Act

--- Program.cs
+++ Program.cs
@@ -4,19 +4,18 @@ using System;
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Menu Version 1");
 Console.WriteLine("Cambios realizados desde Visual Studio .NET");
-
-console.wirte("Ingresa tu nombre")
-string nombre = console.ReadLine();
-console.foregroundcolor = consolecolor.green;
-console.writeLine("Hola " + nombre);
-console.resetcolor();
-
 Console.WriteLine("SISTEMA DE CALCULO");
 Console.WriteLine("Version:1.1");
 Console.WriteLine($"Fecha y hora de incio: {DateTime.Now}");
 Console.WriteLine();
 Console.WriteLine("Cambios sincronizados desde Visual Studio .NET");
 Console.WriteLine("Reositorio actualizado correctamente");
+
+console.wirte("Ingresa tu nombre")
+string nombre = console.ReadLine();
+console.foregroundcolor = consolecolor.green;
+console.writeLine("Hola " + nombre);
+console.resetcolor();
 
 Console.ForegroundColor = ConsoleColor.Yellow;
 Console.WriteLine("presiona cualquier tecla para salir de democalc..");
