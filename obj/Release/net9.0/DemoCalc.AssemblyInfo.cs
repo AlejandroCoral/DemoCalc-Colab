de5aa0ba9260347c67c673740774d527f5ffb6d6
KevRocha <kevin.rocha50@gmail.com>
2026-02-26 00:24:16 -0500

    Actualizacion Kevin Rocha

--- obj/Release/net9.0/DemoCalc.AssemblyInfo.cs
+++ obj/Release/net9.0/DemoCalc.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DemoCalc")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a5b8c87d1b80bd3147b3f363a57714705f1d3644")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aa6a74f50ab644e6bfee73677a1bada5b957140d")]
 [assembly: System.Reflection.AssemblyProductAttribute("DemoCalc")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DemoCalc")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
