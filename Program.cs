4922346e2cc20da39fe69ec9ce0617154034d179
MasalemaCristian <masalemac@gmail.com>
2026-02-26 01:55:26 -0500

    actualizacion Cristian Masalema

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System;
+
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Menu Version 1");
 Console.WriteLine("Cambios realizados desde Visual Studio .NET");
@@ -16,3 +18,7 @@ Console.WriteLine();
 Console.WriteLine("Cambios sincronizados desde Visual Studio .NET");
 Console.WriteLine("Reositorio actualizado correctamente");
 
+Console.ForegroundColor = ConsoleColor.Yellow;
+Console.WriteLine("presiona cualquier tecla para salir de democalc..");
+Console.ResetColor();
+Console.readkey();
