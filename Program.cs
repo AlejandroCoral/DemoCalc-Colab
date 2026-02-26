6f0268ad1f979448c93b414437ae6af8a095d5cb
Matias Fernando Alvarado Pinos <matias2321160@gmail.com>
2026-02-26 02:13:27 -0500

    Final

--- Program.cs
+++ Program.cs
@@ -1,9 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
-using System;
-
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Menu Version 1");
 Console.WriteLine("Cambios realizados desde Visual Studio .NET");
+
 Console.WriteLine("SISTEMA DE CALCULO");
 Console.WriteLine("Version:1.1");
 Console.WriteLine($"Fecha y hora de incio: {DateTime.Now}");
@@ -11,13 +10,13 @@ Console.WriteLine();
 Console.WriteLine("Cambios sincronizados desde Visual Studio .NET");
 Console.WriteLine("Reositorio actualizado correctamente");
 
-console.wirte("Ingresa tu nombre")
-string nombre = console.ReadLine();
-console.foregroundcolor = consolecolor.green;
-console.writeLine("Hola " + nombre);
-console.resetcolor();
+Console.Write("Ingresa tu nombre: ");
+string nombre = Console.ReadLine();
+Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine("Hola " + nombre);
+Console.ResetColor();
 
 Console.ForegroundColor = ConsoleColor.Yellow;
 Console.WriteLine("presiona cualquier tecla para salir de democalc..");
 Console.ResetColor();
-Console.readkey();
+Console.ReadKey();
--- obj/Release/net9.0/DemoCalc.AssemblyInfo.cs
+++ obj/Release/net9.0/DemoCalc.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DemoCalc")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+de5aa0ba9260347c67c673740774d527f5ffb6d6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b861ccda2ada6c756ecb0ced3cdeb09025b5810e")]
 [assembly: System.Reflection.AssemblyProductAttribute("DemoCalc")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DemoCalc")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
