04b75e7cb423af9c8713f15c35d6432b4716d22d
AlejoGit <coral.alejandro05@gmail.com>
2026-02-25 20:36:33 -0500

    Update from Visual Studio

--- Program.cs
+++ Program.cs
@@ -1,3 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Menu Version 1");
+Console.WriteLine("Cambios realizados desde Visual Studio .NET");
