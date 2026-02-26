7cf96b2c30dbf0f4c75bc5714d224bb07f31621d
Matias Fernando Alvarado Pinos <matias2321160@gmail.com>
2026-02-26 01:20:35 -0500

    Actualizacion

--- Program.cs
+++ Program.cs
@@ -5,22 +5,4 @@ Console.WriteLine("Hello, World!");
 Console.WriteLine("Menu Version 1");
 Console.WriteLine("Cambios realizados desde Visual Studio .NET");
 
-Console.Write("Ingresa tu nombre: ");
-string nombre = Console.ReadLine();
-
-Console.ForegroundColor = ConsoleColor.Green;
-Console.WriteLine($"¡Hola {nombre}!");
-Console.ResetColor();
-
-Console.WriteLine("   SISTEMA DE CÁLCULO - DEMOCALC   ");
-Console.WriteLine("Versión: 1.1");
-Console.WriteLine($"Fecha y hora de inicio: {DateTime.Now}");
-Console.WriteLine();
-Console.WriteLine("Cambios sincronizados desde Visual Studio .NET");
-Console.WriteLine("Repositorio actualizado correctamente");
-
-Console.ForegroundColor = ConsoleColor.Yellow;
-Console.WriteLine("Presiona cualquier tecla para salir de DemoCalc...");
-Console.ResetColor();
-Console.ReadKey();
 
