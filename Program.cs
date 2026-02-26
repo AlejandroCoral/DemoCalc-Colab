84515b47be9166fda0012cb503ef5b1da9f985bc
KevRocha <kevin.rocha50@gmail.com>
2026-02-26 01:46:28 -0500

    Actualizacion Rocha Kevin

--- Program.cs
+++ Program.cs
@@ -9,3 +9,10 @@ console.foregroundcolor = consolecolor.green;
 console.writeLine("Hola " + nombre);
 console.resetcolor();
 
+Console.WriteLine("SISTEMA DE CALCULO");
+Console.WriteLine("Version:1.1");
+Console.WriteLine($"Fecha y hora de incio: {DateTime.Now}");
+Console.WriteLine();
+Console.WriteLine("Cambios sincronizados desde Visual Studio .NET");
+Console.WriteLine("Reositorio actualizado correctamente");
+
