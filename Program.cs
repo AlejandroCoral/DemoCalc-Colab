de16d507775ea7863274681fb893637ce6da4e54
Invitado QA <invitado@gmail.com>
2026-02-25 20:16:38 -0500

    Add Validation module by AQ member

--- Program.cs
+++ Program.cs
@@ -1,3 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Menu Version 1");
+Console.WriteLine("Validaciones agregadas por integrante QA");
