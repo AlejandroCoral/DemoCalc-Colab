470dc4c3d92346354579c47785b3a57dc2c16d2a
MasalemaCristian <masalemac@gmail.com>
2026-02-26 00:33:12 -0500

    Actualizacion Masalema

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System;
+
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Menu Version 1");
 Console.WriteLine("Cambios realizados desde Visual Studio .NET");
@@ -15,3 +17,7 @@ Console.WriteLine($"Fecha y hora de inicio: {DateTime.Now}");
 Console.WriteLine();
 Console.WriteLine("Cambios sincronizados desde Visual Studio .NET");
 Console.WriteLine("Repositorio actualizado correctamente");
+Console.ForegroundColor = ConsoleColor.Yellow;
+Console.WriteLine("Presiona cualquier tecla para salir de DemoCalc...");
+Console.ResetColor();
+Console.ReadKey();
--- obj/Debug/net9.0/DemoCalc.AssemblyInfo.cs
+++ obj/Debug/net9.0/DemoCalc.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DemoCalc")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aa6a74f50ab644e6bfee73677a1bada5b957140d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+de5aa0ba9260347c67c673740774d527f5ffb6d6")]
 [assembly: System.Reflection.AssemblyProductAttribute("DemoCalc")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DemoCalc")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
