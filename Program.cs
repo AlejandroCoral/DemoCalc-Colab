24b5dec78f9933b9bed4d38dd3cccc2bf9088560
Matias Fernando Alvarado Pinos <matias2321160@gmail.com>
2026-02-26 01:39:50 -0500

    Actualizacion Alvarado Matias

--- Program.cs
+++ Program.cs
@@ -3,4 +3,9 @@ Console.WriteLine("Hello, World!");
 Console.WriteLine("Menu Version 1");
 Console.WriteLine("Cambios realizados desde Visual Studio .NET");
 
+console.wirte("Ingresa tu nombre")
+string nombre = console.ReadLine();
+console.foregroundcolor = consolecolor.green;
+console.writeLine("Hola " + nombre);
+console.resetcolor();
 
